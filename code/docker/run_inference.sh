7a79eec7a16aa47228d6d3d881d491fc0cce3d72
Carlijn Lems <carlijn.lems@hotmail.nl>
2025-09-04 14:23:14 +0200

    Fix relative paths

--- code/docker/run_inference.sh
+++ code/docker/run_inference.sh
@@ -4,7 +4,7 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 # Navigate to the project root directory
-cd "$SCRIPT_DIR/../../.."
+cd "$SCRIPT_DIR/../.."
 
 docker run --rm \
     -v "$(pwd)":/beetle \
